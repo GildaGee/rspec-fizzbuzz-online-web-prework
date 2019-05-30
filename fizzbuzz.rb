230f02d71423c504c7b315fafb15db4b2d4f680e
Gilda Snow <gilda.snow@gmail.com>
2019-05-30 23:00:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,17 @@
 def fizzbuzz(int)
   if int % 3 == 0 
     puts "Fizz"
+  
   elsif 
   int % 5 == 0 
   puts "Buzz"
+  
+  elsif 
+  int % 5*3 == 0 
+  puts "FizzBuzz"
+  
+else 
+  puts ''
     
  end 
 end
